cd4a182b6dfdfac16917d5b45308f1043f96a5b0
izabela <izabela.leme@plasutil.com.br>
2023-02-10 10:19:32 -0300

    first commit

---
+++ EtqCor/Frm000100EtqTestesCor.cs
@@ -0,0 +1,28 @@
+﻿/* Projeto: Etiqueta Testes de Cor
+ * Autora: Izabela Leme
+ * Data: 10/02/2023
+ * */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections;
+
+namespace EtqCor
+{
+    public partial class Frm000100EtqTestesCor : Form
+    {
+        public Frm000100EtqTestesCor()
+        {
+            InitializeComponent();
+        }
+    }
+}
