73f38f76b3b461fa95588163ffb9454220bc9582
izabela <izabela.leme@plasutil.com.br>
2023-02-10 10:39:21 -0300

    Desenvolvendo o Layout

--- EtqCor/Frm000100EtqTestesCor.Designer.cs
+++ EtqCor/Frm000100EtqTestesCor.Designer.cs
@@ -32,7 +32,7 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Frm000100EtqTestesCor));
             this.containerPanel1 = new TIPlasutil.Componentes.ContainerPanel();
             this.pnlContent = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
+            this.lblPigmento = new System.Windows.Forms.Label();
             this.cbxPigmento = new Plasutil.Components.CustomBox();
             this.lblCodCor = new System.Windows.Forms.Label();
             this.cbxCodCor = new Plasutil.Components.CustomBox();
@@ -42,8 +42,13 @@
             this.cbxFornecedor = new Plasutil.Components.CustomBox();
             this.lblResina = new System.Windows.Forms.Label();
             this.cbxResina = new Plasutil.Components.CustomBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.lblImpressora = new System.Windows.Forms.Label();
+            this.cboImpressora = new System.Windows.Forms.ComboBox();
+            this.lblTamanho = new System.Windows.Forms.Label();
+            this.cboTamanho = new System.Windows.Forms.ComboBox();
+            this.lblQntde = new System.Windows.Forms.Label();
+            this.cbxQntdeImpressao = new Plasutil.Components.CustomBox();
+            this.chkUpper = new System.Windows.Forms.CheckBox();
             this.pnlContent.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -70,33 +75,38 @@
             // pnlContent
             // 
             this.pnlContent.AutoSize = true;
-            this.pnlContent.Controls.Add(this.comboBox1);
+            this.pnlContent.Controls.Add(this.chkUpper);
+            this.pnlContent.Controls.Add(this.cbxQntdeImpressao);
+            this.pnlContent.Controls.Add(this.cboTamanho);
+            this.pnlContent.Controls.Add(this.cboImpressora);
             this.pnlContent.Controls.Add(this.cbxResina);
             this.pnlContent.Controls.Add(this.cbxFornecedor);
             this.pnlContent.Controls.Add(this.cbxConcentracao);
             this.pnlContent.Controls.Add(this.cbxCodCor);
             this.pnlContent.Controls.Add(this.cbxPigmento);
-            this.pnlContent.Controls.Add(this.label2);
+            this.pnlContent.Controls.Add(this.lblQntde);
+            this.pnlContent.Controls.Add(this.lblTamanho);
+            this.pnlContent.Controls.Add(this.lblImpressora);
             this.pnlContent.Controls.Add(this.lblResina);
             this.pnlContent.Controls.Add(this.lblFornecedor);
             this.pnlContent.Controls.Add(this.lblConcentracao);
             this.pnlContent.Controls.Add(this.lblCodCor);
-            this.pnlContent.Controls.Add(this.label1);
+            this.pnlContent.Controls.Add(this.lblPigmento);
             this.pnlContent.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pnlContent.Location = new System.Drawing.Point(0, 0);
             this.pnlContent.Name = "pnlContent";
             this.pnlContent.Size = new System.Drawing.Size(798, 428);
             this.pnlContent.TabIndex = 1;
             // 
-            // label1
+            // lblPigmento
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(11, 16);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(70, 14);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Pigmento:";
+            this.lblPigmento.AutoSize = true;
+            this.lblPigmento.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPigmento.Location = new System.Drawing.Point(11, 16);
+            this.lblPigmento.Name = "lblPigmento";
+            this.lblPigmento.Size = new System.Drawing.Size(70, 14);
+            this.lblPigmento.TabIndex = 0;
+            this.lblPigmento.Text = "Pigmento:";
             // 
             // cbxPigmento
             // 
@@ -105,7 +115,7 @@
             this.cbxPigmento.Formato = Plasutil.Components.Tipo.AlfaNumérico;
             this.cbxPigmento.Location = new System.Drawing.Point(112, 14);
             this.cbxPigmento.Name = "cbxPigmento";
-            this.cbxPigmento.Size = new System.Drawing.Size(259, 20);
+            this.cbxPigmento.Size = new System.Drawing.Size(253, 20);
             this.cbxPigmento.TabIndex = 1;
             // 
             // lblCodCor
@@ -125,7 +135,7 @@
             this.cbxCodCor.Formato = Plasutil.Components.Tipo.AlfaNumérico;
             this.cbxCodCor.Location = new System.Drawing.Point(112, 39);
             this.cbxCodCor.Name = "cbxCodCor";
-            this.cbxCodCor.Size = new System.Drawing.Size(259, 20);
+            this.cbxCodCor.Size = new System.Drawing.Size(253, 20);
             this.cbxCodCor.TabIndex = 2;
             // 
             // lblConcentracao
@@ -145,7 +155,7 @@
             this.cbxConcentracao.Formato = Plasutil.Components.Tipo.AlfaNumérico;
             this.cbxConcentracao.Location = new System.Drawing.Point(112, 64);
             this.cbxConcentracao.Name = "cbxConcentracao";
-            this.cbxConcentracao.Size = new System.Drawing.Size(259, 20);
+            this.cbxConcentracao.Size = new System.Drawing.Size(253, 20);
             this.cbxConcentracao.TabIndex = 3;
             // 
             // lblFornecedor
@@ -165,7 +175,7 @@
             this.cbxFornecedor.Formato = Plasutil.Components.Tipo.AlfaNumérico;
             this.cbxFornecedor.Location = new System.Drawing.Point(112, 89);
             this.cbxFornecedor.Name = "cbxFornecedor";
-            this.cbxFornecedor.Size = new System.Drawing.Size(259, 20);
+            this.cbxFornecedor.Size = new System.Drawing.Size(253, 20);
             this.cbxFornecedor.TabIndex = 4;
             // 
             // lblResina
@@ -185,26 +195,79 @@
             this.cbxResina.Formato = Plasutil.Components.Tipo.AlfaNumérico;
             this.cbxResina.Location = new System.Drawing.Point(112, 114);
             this.cbxResina.Name = "cbxResina";
-            this.cbxResina.Size = new System.Drawing.Size(259, 20);
+            this.cbxResina.Size = new System.Drawing.Size(253, 20);
             this.cbxResina.TabIndex = 5;
             // 
-            // label2
+            // lblImpressora
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Tahoma", 8.25F);
-            this.label2.Location = new System.Drawing.Point(11, 167);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(65, 13);
-            this.label2.TabIndex = 0;
-            this.label2.Text = "Impressora:";
+            this.lblImpressora.AutoSize = true;
+            this.lblImpressora.Font = new System.Drawing.Font("Tahoma", 8.25F);
+            this.lblImpressora.Location = new System.Drawing.Point(11, 167);
+            this.lblImpressora.Name = "lblImpressora";
+            this.lblImpressora.Size = new System.Drawing.Size(65, 13);
+            this.lblImpressora.TabIndex = 0;
+            this.lblImpressora.Text = "Impressora:";
             // 
-            // comboBox1
+            // cboImpressora
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(96, 164);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
-            this.comboBox1.TabIndex = 6;
+            this.cboImpressora.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboImpressora.FormattingEnabled = true;
+            this.cboImpressora.Location = new System.Drawing.Point(82, 164);
+            this.cboImpressora.Name = "cboImpressora";
+            this.cboImpressora.Size = new System.Drawing.Size(283, 21);
+            this.cboImpressora.TabIndex = 6;
+            // 
+            // lblTamanho
+            // 
+            this.lblTamanho.AutoSize = true;
+            this.lblTamanho.Font = new System.Drawing.Font("Tahoma", 8.25F);
+            this.lblTamanho.Location = new System.Drawing.Point(21, 192);
+            this.lblTamanho.Name = "lblTamanho";
+            this.lblTamanho.Size = new System.Drawing.Size(55, 13);
+            this.lblTamanho.TabIndex = 0;
+            this.lblTamanho.Text = "Tamanho:";
+            // 
+            // cboTamanho
+            // 
+            this.cboTamanho.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboTamanho.DropDownWidth = 107;
+            this.cboTamanho.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboTamanho.FormattingEnabled = true;
+            this.cboTamanho.Location = new System.Drawing.Point(82, 189);
+            this.cboTamanho.Name = "cboTamanho";
+            this.cboTamanho.Size = new System.Drawing.Size(107, 21);
+            this.cboTamanho.TabIndex = 7;
+            // 
+            // lblQntde
+            // 
+            this.lblQntde.AutoSize = true;
+            this.lblQntde.Font = new System.Drawing.Font("Tahoma", 8.25F);
+            this.lblQntde.Location = new System.Drawing.Point(257, 192);
+            this.lblQntde.Name = "lblQntde";
+            this.lblQntde.Size = new System.Drawing.Size(67, 13);
+            this.lblQntde.TabIndex = 0;
+            this.lblQntde.Text = "Quantidade:";
+            // 
+            // cbxQntdeImpressao
+            // 
+            this.cbxQntdeImpressao.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.cbxQntdeImpressao.CasasDecimais = 2;
+            this.cbxQntdeImpressao.Formato = Plasutil.Components.Tipo.AlfaNumérico;
+            this.cbxQntdeImpressao.Location = new System.Drawing.Point(330, 190);
+            this.cbxQntdeImpressao.Name = "cbxQntdeImpressao";
+            this.cbxQntdeImpressao.Size = new System.Drawing.Size(35, 20);
+            this.cbxQntdeImpressao.TabIndex = 8;
+            // 
+            // chkUpper
+            // 
+            this.chkUpper.AutoSize = true;
+            this.chkUpper.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkUpper.Location = new System.Drawing.Point(82, 216);
+            this.chkUpper.Name = "chkUpper";
+            this.chkUpper.Size = new System.Drawing.Size(145, 17);
+            this.chkUpper.TabIndex = 9;
+            this.chkUpper.Text = "Texto em letra maiúscula";
+            this.chkUpper.UseVisualStyleBackColor = true;
             // 
             // Frm000100EtqTestesCor
             // 
@@ -233,8 +296,13 @@
         private System.Windows.Forms.Label lblFornecedor;
         private System.Windows.Forms.Label lblConcentracao;
         private System.Windows.Forms.Label lblCodCor;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label lblPigmento;
+        private System.Windows.Forms.Label lblImpressora;
+        private System.Windows.Forms.ComboBox cboImpressora;
+        private System.Windows.Forms.ComboBox cboTamanho;
+        private System.Windows.Forms.Label lblTamanho;
+        private Plasutil.Components.CustomBox cbxQntdeImpressao;
+        private System.Windows.Forms.Label lblQntde;
+        private System.Windows.Forms.CheckBox chkUpper;
     }
 }
